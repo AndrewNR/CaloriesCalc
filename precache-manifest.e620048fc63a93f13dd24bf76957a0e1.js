4cc814d302b582c9fa17323308215ff99c72f331
AndrewNR <andrey.stop4enko@gmail.com>
2019-09-09 23:20:48 +0300

    Updates

---
+++ precache-manifest.e620048fc63a93f13dd24bf76957a0e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf60e520392b0e3de983b73e630894f",
+    "url": "/CaloriesCalc/index.html"
+  },
+  {
+    "revision": "b76ca60cfd60ef00f389",
+    "url": "/CaloriesCalc/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "7dcc23ed3ac0f01b6632",
+    "url": "/CaloriesCalc/static/css/main.5f871fcd.chunk.css"
+  },
+  {
+    "revision": "b76ca60cfd60ef00f389",
+    "url": "/CaloriesCalc/static/js/2.f90e617b.chunk.js"
+  },
+  {
+    "revision": "7dcc23ed3ac0f01b6632",
+    "url": "/CaloriesCalc/static/js/main.b9c5bcf5.chunk.js"
+  },
+  {
+    "revision": "a40bb262efe0e3ddbbef",
+    "url": "/CaloriesCalc/static/js/runtime~main.99d1fd54.js"
+  }
+]);
